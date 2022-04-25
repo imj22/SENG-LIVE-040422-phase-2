0a95436f3c7cfc7bac2069c1a201ca40a4528cce
imj22 <98693440+imj22@users.noreply.github.com>
2022-04-25 23:26:04 +0200

    spinning frogs in the dom (look at commented out image in App.js as well)

---
+++ 01-intro-to-react/my-project/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://cdn.britannica.com/84/206384-050-00698723/Javan-gliding-tree-frog.jpg" className="App-logo" alt="logo" />
+        <p>
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+          greetings, homosexuals
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {/* Learn React */}
+        </a>
+        {/* <img src="https://data.whicdn.com/images/331316028/original.jpg" className="App-logo" alt="logo" /> */}
+        <img src="https://cdn.britannica.com/84/206384-050-00698723/Javan-gliding-tree-frog.jpg" className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
